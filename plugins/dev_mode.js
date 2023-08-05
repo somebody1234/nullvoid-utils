9990f5c5ba4b15dab4ecf1622d580629c301cd52
somebody1234 <ehern.lee@gmail.com>
2023-08-06 00:29:48 +1000

    Update dev_mode.js

--- plugins/dev_mode.js
+++ plugins/dev_mode.js
@@ -1,4 +1,4 @@
 import { importModule } from '../_import.js';
 
-importModule(`https://u.nullv0.id/plugins/auto_import.js`);
+importModule(`${import.meta.url}/../auto_import.js`);
 if (!document.querySelector('noscript')) { console.warn('no <noscript> tag on page!'); }
