3f0b410fa9237a1a8001d7df004d63212097e188
somebody1234 <ehern.lee@gmail.com>
2023-08-06 00:24:15 +1000

    Update auto_import.js

--- plugins/auto_import.js
+++ plugins/auto_import.js
@@ -1,21 +1,21 @@
 import { importModule, importStyle } from '../_import.js';
 
-const domain = new URL(import.meta.url).host;
+const root = `${import.meta.url}/..`;
 
 let theme = 'theme' in document.body.dataset ? document.body.dataset.theme : document.querySelector('meta[name=nv-theme]')?.content;
 
 if (theme) {
-  importStyle(`https://${domain}/base.css`);
-  importStyle(`https://${domain}/themes/${theme}.css`);
+  importStyle(`${root}/base.css`);
+  importStyle(`${root}/themes/${theme}.css`);
 }
 
 const tags = Array.from(document.getElementsByTagName('*')).reduce((p, c) => { if (c.tagName.startsWith('NV-')) { p.add(c.tagName.slice(3).toLowerCase()); } return p; }, new Set());
 for (const tag of tags) {
   if (['fullscreen', 'center', 'back', 'home', 'title', 'alwaysactive', 'define'].includes(tag)) {
-    importModule(`https://${domain}/components/${tag}.js`);
+    importModule(`${root}/components/${tag}.js`);
   } else if (['hljs', 'rellax'].includes(tag)) {
-    importModule(`https://${domain}/tp/${tag}.js`);
+    importModule(`${root}/tp/${tag}.js`);
   } else {
-    importModule(`https://${domain}/${tag}_view.js`);
+    importModule(`${root}/${tag}_view.js`);
   }
 }
